ab1e19639c97d3ad7d21c4ccb6efaa5284b495d7
NgocRuby <87185694+quangngoc1911@users.noreply.github.com>
2024-05-13 10:59:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
-app.use(express.json());
-app.use(cors());
+
 require('dotenv').config()
 const connectDB = require('./config/connectDB')
 const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
-// const app = express()
+const app = express()
+app.use(express.json());
+app.use(cors());
+
 // app.use(cors({
 //     origin : process.env.FRONTEND_URL,
 //     credentials : true
