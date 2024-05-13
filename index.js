53fadbcaa561abdbe34ee408466608467217a36e
NgocRuby <87185694+quangngoc1911@users.noreply.github.com>
2024-05-13 11:06:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const cookiesParser = require('cookie-parser');
+const connectDB = require('./config/connectDB');
+const router = require('./routes/index');
 
-require('dotenv').config()
-const connectDB = require('./config/connectDB')
-const router = require('./routes/index')
-const cookiesParser = require('cookie-parser')
-const { app, server } = require('./socket/index')
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(cookiesParser());
 
-// app.use(cors({
-//     origin : process.env.FRONTEND_URL,
-//     credentials : true
-// }))
-app.use(express.json())
-app.use(cookiesParser())
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080
-
-app.get('/',(request,response)=>{
+app.get('/', (request, response) => {
     response.json({
-        message : "Server running at " + PORT
-    })
-})
+        message: "Server running at " + PORT
+    });
+});
 
-//api endpoints
-app.use('/api',router)
+// API endpoints
+app.use('/api', router);
 
-connectDB().then(()=>{
-    server.listen(PORT,()=>{
-        console.log("server running at " + PORT)
-    })
-})
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server running at " + PORT);
+    });
+});
