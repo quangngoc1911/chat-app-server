e37cd05b5f585204920952f6c804d6c5903e402f
NgocRuby <87185694+quangngoc1911@users.noreply.github.com>
2024-05-13 11:57:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin :"*",
+    origin :'https://chat-app-client-plum.vercel.app',
     credentials : true
 }))
 app.use(express.json())
