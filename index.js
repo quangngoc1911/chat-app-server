3ebd871aae045a6f34c44c15a8138d6a6292daac
NgocRuby <87185694+quangngoc1911@users.noreply.github.com>
2024-05-13 11:03:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
 const app = express()
-app.use(express.json());
+
 app.use(cors());
 
 // app.use(cors({
